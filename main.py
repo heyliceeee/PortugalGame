a9d549939f677cc4c3e8da676b306be77a10ab86
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-14 17:09:21 +0100

    ✨feat: write district name on map

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import turtle
 from turtle import Screen
+import turtle
 import os
 import pandas
 
 IMAGE = "/data/blank_portugal.gif"
 DATASET = "/data/districts.csv"
 dir_path = os.path.dirname(os.path.abspath(__file__)) # get the path of the current file
+dataset = []
 
 screen = Screen()
 
@@ -31,14 +32,41 @@ def get_data_from_csv():
     """
     file_path = dir_path + DATASET
     return pandas.read_csv(file_path)
+def write_district_name(x, y, district):
+    """
+    write the district name in xy coordinates
+    :param district: district name
+    :param x: x-axis
+    :param y: y-axis
+    """
+    text = turtle.Turtle()
+    text.hideturtle() # hide the turtle
+    text.penup() # put the pen down
+    text.goto(x, y)
+    text.write(district, align="center", font=("Arial", 10, "normal"))
 def game():
     """
     enter the district name until the user wins
     """
-    answer_district = screen.textinput(title="Guess the District", prompt="What's another district's name?").title() # user answer
-    print(answer_district)
+
+    is_game_on = True
+    guessed_districts = []
+    total_districts = len(dataset)
+    while is_game_on:  # while the game happens
+        answer_district = screen.textinput(title=f"{len(guessed_districts)}/{total_districts} Districts Correct", prompt="What's another district's name?").title() # user answer
+
+        if len(dataset[dataset["district"].str.lower() == answer_district.lower()]) >= 1: # check if answer_district exists in the dataset
+            district = dataset[dataset["district"].str.lower() == answer_district.lower()]
+            x = district["x"].values[0]
+            y = district["y"].values[0]
+            write_district_name(x, y, district["district"].values[0]) # write the district name in xy given
+            guessed_districts.append(district)
+
+            if total_districts == len(guessed_districts): # check if complete all districts
+                is_game_on = False # the game is over
 
 setup_screen() # set up the screen
 # screen.onscreenclick(get_mouse_click_coor) # get mouse click coordinates
+dataset = get_data_from_csv()
 game() # enter the district name until the user wins
 screen.mainloop() # keep the screen running
