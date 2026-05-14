d4b0c8ef0bb679feeb0bf238226e9cb3443c9c14
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-14 16:15:10 +0100

    ✨feat: read csv

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import turtle
 from turtle import Screen
-IMAGE = "blank_portugal.gif"
+import os
+import pandas
 
+IMAGE = "/data/blank_portugal.gif"
+DATASET = "/data/districts.csv"
+dir_path = os.path.dirname(os.path.abspath(__file__)) # get the path of the current file
 screen = Screen()
 
 def setup_screen():
@@ -9,8 +13,8 @@ def setup_screen():
     set up the screen
     """
     screen.title("Portugal District Game") # set the title of the screen
-    screen.addshape(IMAGE) # set background image
-    turtle.shape(IMAGE)
+    screen.addshape(dir_path + IMAGE) # set background image
+    turtle.shape(dir_path + IMAGE)
 def get_mouse_click_coor(x, y):
     """
     get the mouse click coordinates
@@ -19,8 +23,15 @@ def get_mouse_click_coor(x, y):
     :return: the mouse click coordinates
     """
     print(x, y)
-
+def get_data_from_csv():
+    """
+    get data from csv file
+    :return: list data
+    """
+    file_path = dir_path + DATASET
+    return pandas.read_csv(file_path)
 
 setup_screen() # set up the screen
-screen.onscreenclick(get_mouse_click_coor) # get mouse click coordinates
+# screen.onscreenclick(get_mouse_click_coor) # get mouse click coordinates
+print(get_data_from_csv())
 screen.mainloop() # keep the screen running
